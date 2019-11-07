a35816bdd3c4ff5fb55f15b65a51a088c291c1d6
Tim Kim <timkimdesigns@gmail.com>
2019-11-07 10:46:06 -0800

    integrated firebase into app and created signin page with sign in component and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,10 +51,12 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             required
           />
-          <CustomButton type="submit">SIGN IN</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            {' '}Sign in with Google{' '}
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">SIGN IN</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );

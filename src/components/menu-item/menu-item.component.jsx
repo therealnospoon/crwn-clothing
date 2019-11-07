0e03a96ab4a21d249b9c918eb0d2542064202c4e
Tim Kim <timkimdesigns@gmail.com>
2019-11-07 08:09:44 -0800

    styled inputs for sign-in form

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-import './menu-item.styles.scss';
+import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-        <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}} />
-        <div className="content">
-            <h1 className="title">{ title.toUpperCase() }</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
+  <div
+    className={`menu-item ${size}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-)
+  </div>
+);
 
 export default withRouter(MenuItem);
